80ddc3df36c93178137ecfe236abcc7380d64a29
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2024-01-02 19:41:45 -0500

    date formatting

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ df = pd.DataFrame(data)
 info = df.info()
 describe = df.describe()
 
-print(df)
-
 #Sort Data By date
 df['Date']=pd.to_datetime(df['Date'], utc=True)
 df['date'] = [d.date() for d in df['Date']]
+correct_date=pd.to_datetime(df['date'], format='%Y-%m-%d').dt.strftime('%m-%d-%Y')
+df1=pd.DataFrame(correct_date)
+df['date']=df1['date'].astype('datetime64[ns]')
+df2=df['date'].dt.year
+df['year']=df2.astype('string')
 print(df)
